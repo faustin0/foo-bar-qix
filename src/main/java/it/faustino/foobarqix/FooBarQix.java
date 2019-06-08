4dafbf96f4312976143b218566dea8705320617e
faustino <dinatalefausto@gmail.com>
2019-06-09 00:33:30 +0200

    refactoring for function factory

--- src/main/java/it/faustino/foobarqix/FooBarQix.java
+++ src/main/java/it/faustino/foobarqix/FooBarQix.java
@@ -2,6 +2,7 @@ package it.faustino.foobarqix;
 
 import java.util.HashMap;
 import java.util.IdentityHashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -9,82 +10,55 @@ import java.util.function.Predicate;
 public class FooBarQix {
 
 
-    private Map<Predicate<Integer>, Function<String, String>> decorations;
+    private Map<Predicate<Integer>, Function<Integer, String>> decorations;
     public static final String startingString = "";
 
     private FooBarQix() {
-        this.decorations = new HashMap<>();
+        this.decorations = new LinkedHashMap<>();
     }
 
     public static FooBarQix create() {
         return new FooBarQix();
     }
 
-    public FooBarQix when(Predicate<Integer> condition, Function<String, String> action) {
+    public FooBarQix when(Predicate<Integer> condition, Function<Integer, String> action) {
         this.decorations.put(condition, action);
         return this;
     }
 
     public String emit(int param) {
 
-        Function<String, String> fooBarQixDecorator = decorations.keySet().stream()
+        String res = decorations.keySet().stream()
                 .filter(condition -> condition.test(param))
                 .map(condition -> decorations.get(condition))
-                .reduce(Function::andThen)
-                .orElseGet(() -> s -> String.valueOf(param));
+                .map(fun -> fun.apply(param))
+                .reduce(String::concat)
+                .orElseGet(() -> String.valueOf(param));
 
-        return fooBarQixDecorator.apply(startingString);
+        return res;
     }
-
-    public String validateFor5(int toValidate, String currentResult) {
-        if (isDivisibleBy(toValidate, 5)) {
-            currentResult += "Bar";
-            return currentResult;
-        }
-        return currentResult;
-    }
-
-    public String validateFor3(int toValidate, String currentResult) {
-        if (isDivisibleBy(toValidate, 3)) {
-            currentResult = "Foo";
-            return currentResult;
-        }
-        return currentResult;
-    }
-
-    public String validateFor7(int toValidate, String currentResult) {
-        if (isDivisibleBy(toValidate, 7)) {
-            currentResult += "Qix";
-            return currentResult;
-        }
-        return currentResult;
-    }
-
 
     public boolean isDivisibleBy(int dividend, int divisor) {
         return dividend % divisor == 0;
     }
 
 
-    public String addsFoo(int i, Predicate<String> isSearchedNumber, Function<String, String> func) {
-        return String.valueOf(i).codePoints()
-                .mapToObj(p -> (char) p)
-                .map(String::valueOf)
-                .filter(s -> s.equals("3"))
-                //.filter(isSearchedNumber)
-                .map(s -> "Foo")
-                .reduce(String::concat)
-                .orElse("");
-    }
-
-    public Function<Integer, String> decorateWith(Predicate<String> condition, Function<String, String> func) {
-        return i -> String.valueOf(i)
+    //TODO change name
+    public static Function<Integer, String> decorateWith(String condition, String toAdd) {
+        return i -> i.toString()
                 .codePoints()
                 .mapToObj(p -> (char) p)
                 .map(String::valueOf)
-                .filter(condition)
-                .map(func)
-                .reduce(String::concat)
-                .orElse("");
+                .filter(s -> s.equals(condition))
+                .map(s -> toAdd)
+                .reduce("", String::concat);
+    }
+
+    public static Predicate<Integer> containsNumber(int toSearch) {
+        return input -> String.valueOf(input)
+                .codePoints()
+                .mapToObj(p -> (char) p)
+                .map(String::valueOf)
+                .anyMatch(s -> s.equals(String.valueOf(toSearch)));
     }
 }
--- src/test/java/it/faustino/foobarqix/FooBarQixTest.java
+++ src/test/java/it/faustino/foobarqix/FooBarQixTest.java
@@ -11,6 +11,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
@@ -26,24 +27,28 @@ public class FooBarQixTest {
     @Mock
     Predicate<Integer> mockIntPredicate;
     @Mock
-    Function<String, String> mockFun;
+    Function<Integer, String> mockFun;
 
     @BeforeEach
     void setUp() {
         Predicate<Integer> isDivisibleByThree = n -> n % 3 == 0;
-        Function<String, String> writeFoo = s -> "Foo";
+        Function<Integer, String> writeFoo = s -> "Foo";
 
         Predicate<Integer> isDivisibleByFive = n -> n % 5 == 0;
-        Function<String, String> addBar = s -> s + "Bar";
+        Function<Integer, String> addBar = s -> "Bar";
 
         Predicate<Integer> isDivisibleBySeven = n -> n % 7 == 0;
-        Function<String, String> addQuix = s -> s + "Qix";
+        Function<Integer, String> addQuix = s -> "Qix";
+
+        Predicate<Integer> containsThree = n -> FooBarQix.containsNumber(3).test(n);
+        Function<Integer, String> addFoo = s -> FooBarQix.decorateWith("3", "Foo").apply(s);
 
         sut = FooBarQix
                 .create()
                 .when(isDivisibleByThree, writeFoo)
                 .when(isDivisibleByFive, addBar)
-                .when(isDivisibleBySeven, addQuix);
+                .when(isDivisibleBySeven, addQuix)
+                .when(containsThree, addFoo);
     }
 
     @AfterEach
@@ -57,54 +62,49 @@ public class FooBarQixTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {3, 6, 9, 12})
-    void shouldWrite_Foo(int number) {
-        var result = sut.emit(number);
-        assertThat(result).isEqualTo("Foo");
-
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {5, 10, 20})
-    void shouldWrite_Bar(int number) {
-        var result = sut.emit(number);
-        assertThat(result).isEqualTo("Bar");
-    }
-
-    @ParameterizedTest
     @ValueSource(ints = {1, 2, 4})
     void shouldWriteTheNumber(int number) {
-        var result = sut.emit(number);
+        String result = sut.emit(number);
         assertThat(result).isEqualTo(String.valueOf(number));
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {15, 30})
-    void shouldWrite_FooBar(int number) {
-        var result = sut.emit(number);
-        assertThat(result).isEqualTo("FooBar");
+    @ValueSource(ints = {6, 9, 12})
+    void shouldWrite_Foo_divisibleByThree(int number) {
+        String result = sut.emit(number);
+        assertThat(result).isEqualTo("Foo");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {5, 10, 20})
+    void shouldWrite_Bar_divisibleByFive(int number) {
+        String result = sut.emit(number);
+        assertThat(result).isEqualTo("Bar");
     }
 
     @ParameterizedTest
     @ValueSource(ints = {7, 14, 28})
     void shouldWrite_Qix(int number) {
-        var result = sut.emit(number);
+        String result = sut.emit(number);
         assertThat(result).isEqualTo("Qix");
     }
 
     @ParameterizedTest
     @ValueSource(ints = {21})
     void shouldAdd_Qix(int number) {
-        var result = sut.emit(number);
+        String result = sut.emit(number);
         assertThat(result).isEqualTo("FooQix");
     }
 
     @Test
     void shouldExecuteRegisteredDecorations() {
-
         Mockito.doReturn(true).
                 when(mockIntPredicate)
                 .test(Mockito.any());
+
+        Mockito.doReturn("").
+                when(mockFun)
+                .apply(Mockito.any());
 
         FooBarQix.create()
                 .when(mockIntPredicate, mockFun)
@@ -115,26 +115,25 @@ public class FooBarQixTest {
     }
 
     @Test
-    void shouldCreateDecoratorFunction() {
-        Mockito.doReturn(true).
-                when(mockStringPredicate)
-                .test(Mockito.any());
-        Mockito.doReturn("").
-                when(mockFun)
-                .apply(Mockito.anyString());
-
-        var decorator = sut.decorateWith(mockStringPredicate, mockFun);
-        decorator.apply(3);
-
-        Mockito.verify(mockStringPredicate).test(Mockito.any());
-        Mockito.verify(mockFun).apply(Mockito.any());
+    void shouldBeTrue_IfContainsNumber() {
+        int input = 35643;
+        int toSearch = 3;
+        boolean test = FooBarQix.containsNumber(toSearch).test(input);
+        assertThat(test).isTrue();
     }
 
-   /* @ParameterizedTest
-    @ValueSource(ints = {3345, 33, 1133, 345365})
-    void shouldReturn_FooForEachThree(int number) {
-        var result = sut.addsFoo(number);
-        System.out.println(result);
-        assertThat(result).isEqualTo("FooFoo");
-    }*/
+    @Test
+    void shouldBeFalse_IfNotContainsNumber() {
+        int input = 35643;
+        int toSearch = 1;
+        boolean test = FooBarQix.containsNumber(toSearch).test(input);
+        assertThat(test).isFalse();
+    }
+
+    @Test
+    void shouldReturn_FooFooFoo() {
+        String result = sut.emit(33);
+
+        assertThat(result).isEqualTo("FooFooFoo");
+    }
 }
