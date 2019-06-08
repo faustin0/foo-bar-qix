f41d106118f38c0d3446dde46fe6a87b477fa372
faustino <dinatalefausto@gmail.com>
2019-06-08 11:23:19 +0200

    fixing maven tests requirements

--- src/test/java/it/faustino/foobarqix/FooBarQixTest.java
+++ src/test/java/it/faustino/foobarqix/FooBarQixTest.java
@@ -1,12 +1,12 @@
 package it.faustino.foobarqix;
 
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -36,6 +36,11 @@ public class FooBarQixTest {
                 .when(isDivisibleByThree, writeFoo)
                 .when(isDivisibleByFive, addBar)
                 .when(isDivisibleBySeven, addQuix);
+    }
+
+    @AfterEach
+    void tearDown() {
+        sut = FooBarQix.create();
     }
 
     @Test
